03d58c4f0f514110f3ab85ef6764514cdaaa406e
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-08 23:33:56 +0300

    Create service to get player location

---
+++ Source/GMTKjam2023/Enemies/Behavior/BTService_GetPlayerLocation.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_GetPlayerLocation.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UBTService_GetPlayerLocation::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+
+	if (Blackboard)
+	{
+		AActor* TargetActor = Cast<AActor>(Blackboard->GetValueAsObject(PlayerActorKey.SelectedKeyName));
+
+		if (TargetActor)
+		{
+			Blackboard->SetValueAsVector(TargetLocationKey.SelectedKeyName, TargetActor->GetActorLocation());
+		}
+	}
+	
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
