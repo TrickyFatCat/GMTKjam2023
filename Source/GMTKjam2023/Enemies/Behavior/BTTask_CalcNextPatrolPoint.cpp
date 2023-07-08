d6b43ad7874136dddb584b1a850a93ebab96bb0d
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-08 21:41:15 +0300

    Create BT patrolling tasks

---
+++ Source/GMTKjam2023/Enemies/Behavior/BTTask_CalcNextPatrolPoint.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_CalcNextPatrolPoint.h"
+
+#include "GMTKjam2023/Components/EnemyPatrolManager.h"
+#include "AIController.h"
+
+EBTNodeResult::Type UBTTask_CalcNextPatrolPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AActor* Owner = Cast<AAIController>(OwnerComp.GetOwner())->GetPawn();
+
+	if (!Owner)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UEnemyPatrolManager* PatrolManager = Owner->FindComponentByClass<UEnemyPatrolManager>();
+
+	if (!PatrolManager)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	PatrolManager->CalculateNextPatrolPoint();
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/GMTKjam2023/Enemies/Behavior/BTTask_GetPatrolLocation.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_GetPatrolLocation.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GMTKjam2023/Components/EnemyPatrolManager.h"
+
+UBTTask_GetPatrolLocation::UBTTask_GetPatrolLocation()
+{
+}
+
+EBTNodeResult::Type UBTTask_GetPatrolLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+	AActor* Owner = Cast<AAIController>(OwnerComp.GetOwner())->GetPawn();
+	
+	if (!Blackboard || !Owner)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UEnemyPatrolManager* PatrolManager = Owner->FindComponentByClass<UEnemyPatrolManager>();
+
+	if (!PatrolManager)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	FVector TargetLocation {FVector::ZeroVector};
+
+	if (!PatrolManager->GetPatrolLocation(TargetLocation))
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	Blackboard->SetValueAsVector(TargetLocationKey.SelectedKeyName, TargetLocation);
+
+	return EBTNodeResult::Succeeded;
+}
