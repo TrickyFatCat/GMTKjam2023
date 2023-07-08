e64132514fdef2bff85276eb23c96d1cd392a9d1
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-09 00:31:09 +0300

    Create attack component

---
+++ Source/GMTKjam2023/Components/AttackComponent.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttackComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+UAttackComponent::UAttackComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UAttackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+float UAttackComponent::StartAttack()
+{
+	if (!AttackMontage)
+	{
+		return 0.f;
+	}
+
+	ACharacter* Character = Cast<ACharacter>(GetOwner());
+
+	if (!Character)
+	{
+		return 0.f;
+	}
+
+	return Character->PlayAnimMontage(AttackMontage);
+}
---
+++ Source/GMTKjam2023/Components/AttackComponent.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AttackComponent.generated.h"
+
+class UAnimMontage;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTKJAM2023_API UAttackComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UAttackComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UAnimMontage* AttackMontage = nullptr;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	float StartAttack();
+};
