8f219f9723888c519fb326a15acba8003a21306c
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-08 15:28:12 +0300

    Create lure pickup class

---
+++ Source/GMTKjam2023/GameplayObjects/LurePickup.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LurePickup.h"
+
+#include "SphereInteractionComponent.h"
+
+
+ALurePickup::ALurePickup()
+{
+}
+
+void ALurePickup::DisablePickup()
+{
+}
+
+void ALurePickup::StartInteraction_Implementation(AActor* OtherActor)
+{
+	InteractionTriggerComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+bool ALurePickup::FinishInteraction_Implementation(AActor* OtherActor)
+{
+	InteractionTriggerComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	return Super::FinishInteraction_Implementation(OtherActor);
+}
+
---
+++ Source/GMTKjam2023/GameplayObjects/LurePickup.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupInteractive.h"
+#include "LurePickup.generated.h"
+
+UCLASS()
+class GMTKJAM2023_API ALurePickup : public APickupInteractive
+{
+	GENERATED_BODY()
+
+public:
+	ALurePickup();
+
+protected:
+	virtual void DisablePickup() override;
+
+private:
+	virtual void StartInteraction_Implementation(AActor* OtherActor) override;
+
+	virtual bool FinishInteraction_Implementation(AActor* OtherActor) override;
+};
+
