4dbabce4f03e7e8448dfa6069d97f22deeabce25
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-08 20:09:55 +0300

    Create enemy character class

---
+++ Source/GMTKjam2023/Enemies/EnemyCharacter.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyCharacter.h"
+
+#include "GMTKjam2023/Components/HitPointsComponent.h"
+
+
+AEnemyCharacter::AEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitPoints = CreateDefaultSubobject<UHitPointsComponent>("HitPoints");
+}
+
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/GMTKjam2023/Enemies/EnemyCharacter.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+class UHitPointsComponent;
+
+UCLASS()
+class GMTKJAM2023_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UHitPointsComponent> HitPoints = nullptr;
+};
