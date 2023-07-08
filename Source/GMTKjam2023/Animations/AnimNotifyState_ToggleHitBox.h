32a5a3200cedc2cad1aea9f6bc594a72e8f0b194
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-09 01:35:59 +0300

    Create anim notify state for melee attacks

---
+++ Source/GMTKjam2023/Animations/AnimNotifyState_ToggleHitBox.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_ToggleHitBox.generated.h"
+
+class UMeleeHitBox;
+/**
+ * 
+ */
+UCLASS()
+class GMTKJAM2023_API UAnimNotifyState_ToggleHitBox : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	UPROPERTY()
+	UMeleeHitBox* HitBox = nullptr;
+};
