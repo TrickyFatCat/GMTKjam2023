859da3680b87678f37b24caa2a8420eca4b08853
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-08 23:15:54 +0300

    Create player handler component

---
+++ Source/GMTKjam2023/Components/EnemyPlayerHandler.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyPlayerHandler.h"
+
+#include "AIController.h"
+#include "MimicHandlerComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GMTKjam2023/Player/PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UEnemyPlayerHandler::UEnemyPlayerHandler()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UEnemyPlayerHandler::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerCharacter = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0));
+
+	if (PlayerCharacter)
+	{
+		MimicHandler = PlayerCharacter->FindComponentByClass<UMimicHandlerComponent>();
+	}
+
+	AAIController* Controller = Cast<AAIController>(GetOwner()->GetInstigatorController());
+
+	if (Controller)
+	{
+		Controller->GetBlackboardComponent()->SetValueAsObject(PlayerActorKeyName, PlayerCharacter);
+	}
+}
---
+++ Source/GMTKjam2023/Components/EnemyPlayerHandler.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyPlayerHandler.generated.h"
+
+class APlayerCharacter;
+class UMimicHandlerComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTKJAM2023_API UEnemyPlayerHandler : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyPlayerHandler();
+
+	APlayerCharacter* GetPlayerCharacter() const { return PlayerCharacter; }
+
+	UMimicHandlerComponent* GetMimicHandler() const { return MimicHandler; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	APlayerCharacter* PlayerCharacter = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	UMimicHandlerComponent* MimicHandler = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FName PlayerActorKeyName = "PlayerActor";
+};
