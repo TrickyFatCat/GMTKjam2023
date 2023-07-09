47c7b08d7ca4f89a7c83792f911891cbf69a67d3
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-09 06:59:42 +0300

    Create base projectile class

---
+++ Source/GMTKjam2023/GameplayObjects/ProjectileBase.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+class UMeleeHitBox;
+class UStaticMeshComponent;
+
+UCLASS()
+class GMTKJAM2023_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectileBase();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UMeleeHitBox> HitBox = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly)
+	float MaxDistance = 1024.f;
+
+	FVector InitialLocation{FVector::ZeroVector};
+
+	UFUNCTION()
+	void HandleProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                  AActor* OtherActor,
+	                                  UPrimitiveComponent* OtherComp,
+	                                  int32 OtherBodyIndex,
+	                                  bool bFromSweep,
+	                                  const FHitResult& SweepResult);
+};
