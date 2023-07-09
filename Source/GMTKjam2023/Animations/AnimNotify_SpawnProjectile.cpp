01a87cfaef4efb92bc402184bde77142e4a0679c
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-09 07:06:26 +0300

    Create notify to spawn projectile

---
+++ Source/GMTKjam2023/Animations/AnimNotify_SpawnProjectile.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_SpawnProjectile.h"
+
+#include "GMTKjam2023/GameplayObjects/ProjectileBase.h"
+
+void UAnimNotify_SpawnProjectile::Notify(USkeletalMeshComponent* MeshComp,
+                                         UAnimSequenceBase* Animation,
+                                         const FAnimNotifyEventReference& EventReference)
+{
+	if (!ProjectileClass)
+	{
+		return;
+	}
+
+	FTransform SpawnTransform;
+	SpawnTransform.SetLocation(MeshComp->GetSocketLocation(SpawnSocketName));
+	SpawnTransform.SetRotation(FQuat(MeshComp->GetOwner()->GetActorForwardVector().Rotation()));
+	GetWorld()->SpawnActor<AProjectileBase>(ProjectileClass, SpawnTransform);
+	Super::Notify(MeshComp, Animation, EventReference);
+}
