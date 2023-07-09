6434691873d4667ea5db01aee5ec10e5e3d21f18
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-09 11:35:36 +0300

    Create enemy desire component

---
+++ Source/GMTKjam2023/Components/EnemyDesireComponent.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyDesireComponent.h"
+
+
+UEnemyDesireComponent::UEnemyDesireComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UEnemyDesireComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/GMTKjam2023/Components/EnemyDesireComponent.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GMTKjam2023/GMTKjam2023.h"
+#include "EnemyDesireComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTKJAM2023_API UEnemyDesireComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyDesireComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	ELureType DesiredLure = ELureType::Gold;
+};
